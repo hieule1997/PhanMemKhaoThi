eabc4d1781d793b010b7624f307ff28cf48a7a6f
cuong <duccuongdc97@gmail.com>
2018-11-29 18:11:28 +0700

    cuong update

--- CoiThi/views.py
+++ CoiThi/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
 # Create your views here.
+
+def login(request):
+  return render(request, 'coithi/login.html')
+
+def index(request):
+  return render(request, 'coithi/index.html')
